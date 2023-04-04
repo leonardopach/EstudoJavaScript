51393730d6892dd8be661c895a2c5535b6d4c54b
leonardopach <pachecodossantos17@gmail.com>
2023-04-04 18:29:06 -0300

    react/vue

--- react/calculadora/src/components/Button.jsx
+++ react/calculadora/src/components/Button.jsx
@@ -2,14 +2,19 @@ import React from "react";
 import "./Button.css";
 
 const button = props => {
+    let classes = "button ";
+    classes += props.operation ? "operation" : "";
+    classes += props.double ? "double" : "";
+    classes += props.triple ? "triple" : "";
+
+
     return (
-        < button className={`button
-    ${props.operation ? "operation" : ""}
-    ${props.doubles ? "doubles" : ""}
-    ${props.triples ? "triples" : ""}
-    `} >
+        < button
+            onClick={e => props.click && props.click(props.label)}
+            className={classes} >
             {props.label}
         </button >
     )
+
 }
 export default button;
