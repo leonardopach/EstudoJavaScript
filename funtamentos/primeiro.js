1d0384fd4865f7f36f590e86a92210f169f4bd93
leonardo <pachecodossantos17@gmail.com>
2024-02-06 16:45:30 -0300

    Apenas uma alteracao da troca de valores das variaveis

--- funtamentos/primeiro.js
+++ funtamentos/primeiro.js
@@ -9,3 +9,13 @@ console.log("Hello world");
 for (let i = 0; i <= 10; i++) {
   console.log(i);
 }
+
+let a = 7;
+let b = 94;
+[a, b] = [b, a];
+// let temp = b;
+// b = a;
+// a = temp;
+
+console.log(a);
+console.log(b);
