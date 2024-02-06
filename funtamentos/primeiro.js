ea19f5370f58b00b6fd0466cddff882cfc1bb0d4
leonardo <pachecodossantos17@gmail.com>
2024-02-06 15:01:40 -0300

    Fundamentos de javascript primeiro programa "Hello world"

---
+++ funtamentos/primeiro.js
@@ -0,0 +1,11 @@
+console.log("legal");
+console.log("show");
+console.log(1 + 2);
+console.log("passo 1");
+console.log("Passo 2");
+
+console.log("Hello world");
+
+for (let i = 0; i <= 10; i++) {
+  console.log(i);
+}
