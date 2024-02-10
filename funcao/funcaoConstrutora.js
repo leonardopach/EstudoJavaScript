d87cede6e5dfdec13244bd582e67eb98816b97ea
leonardo <pachecodossantos17@gmail.com>
2024-02-10 13:55:10 -0300

    funcao

--- funcao/funcaoConstrutora.js
+++ funcao/funcaoConstrutora.js
@@ -1,29 +1,28 @@
 function Carro(velocidadeMaxima = 200, delta = 5) {
     // atributo privado
-    let velocidadeAtual = 0
-
+    let velocidadeAtual = 0;
 
     // metedo publico
-    this.acelerar = function() {
+    this.acelerar = function () {
         if (velocidadeAtual + delta <= velocidadeMaxima) {
-            velocidadeAtual += delta
+            velocidadeAtual += delta;
         } else {
-            velocidadeAtual = velocidadeMaxima
+            velocidadeAtual = velocidadeMaxima;
         }
-    }
+    };
 
     // metodo publico
-    this.getVelocidadeAtual = function() {
-        return velocidadeAtual
-    }
+    this.getVelocidadeAtual = function () {
+        return velocidadeAtual;
+    };
 }
 
-const uno = new Carro
-uno.acelerar()
-console.log(uno.getVelocidadeAtual())
+const uno = new Carro();
+uno.acelerar();
+console.log(uno.getVelocidadeAtual());
 
-const ferrari = new Carro(350, 20)
-ferrari.acelerar()
-ferrari.acelerar()
-ferrari.acelerar()
-console.log(ferrari.getVelocidadeAtual())
+const ferrari = new Carro(350, 20);
+ferrari.acelerar();
+ferrari.acelerar();
+ferrari.acelerar();
+console.log(ferrari.getVelocidadeAtual());
