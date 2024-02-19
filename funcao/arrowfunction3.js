2d8b4e6faf40a3e7a2c692f4445152d8b7eae7df
leonardo <pachecodossantos17@gmail.com>
2024-02-19 14:03:15 -0300

    funcao

--- funcao/arrowfunction3.js
+++ funcao/arrowfunction3.js
@@ -1,19 +1,18 @@
-let comparaComThis = function(param) {
-    console.log(this === param)
-}
+let comparaComThis = function (param) {
+    console.log(this === param);
+};
 
-comparaComThis(global)
+comparaComThis(global);
 
-const obj = {}
-comparaComThis = comparaComThis.bind(obj)
-comparaComThis(global)
-comparaComThis(obj)
+const obj = {};
+comparaComThis = comparaComThis.bind(obj);
+comparaComThis(global);
+comparaComThis(obj);
 
+let comparaComThisArrow = param => console.log(this === param);
+comparaComThisArrow(global);
+comparaComThisArrow(module.exports);
 
-let comparaComThisArrow = param => console.log(this === param)
-comparaComThisArrow(global)
-comparaComThisArrow(module.exports)
-
-comparaComThisArrow = comparaComThisArrow.bind(obj)
-comparaComThisArrow(obj)
-comparaComThisArrow(module.exports)
+comparaComThisArrow = comparaComThisArrow.bind(obj);
+comparaComThisArrow(obj);
+comparaComThisArrow(module.exports);
