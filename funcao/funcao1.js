2ef5264d8b27b9d9ef4e5e058bcfd7b825afae8e
leonardo <pachecodossantos17@gmail.com>
2024-02-22 13:29:22 -0300

    funcao

--- funcao/funcao1.js
+++ funcao/funcao1.js
@@ -10,20 +10,16 @@ funcao(1, 2, 3, 4, 5, 6, 7);
 function funcao1(a = 0, b = 0) {
     console.log(a, b);
 }
-
 funcao1(20);
-
 function funcao2({ nome, sobrenome, idade }) {
     console.log(nome, sobrenome, idade);
 }
 let obj = { nome: "Leonardo", sobrenome: "Pacheco", idade: 20 };
 funcao2(obj);
-
 function funcao3([valor1, valor2, valor3]) {
     console.log(valor1, valor2, valor3);
 }
 funcao3(["Leonardo", "Pacheco", 26]);
-
 function conta(operador, acumulador, ...numeros) {
     for (let numero of numeros) {
         if (operador === "+") acumulador += numero;
---
+++ funcao/funcaoGeradora.js
@@ -0,0 +1,10 @@
+function* geradora1() {
+    yield "valor 1";
+    yield "valor 2";
+    yield "valor 3";
+}
+
+const g1 = geradora1();
+console.log(g1.next().value);
+console.log(g1.next().value);
+console.log(g1.next().value);
