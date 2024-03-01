6ac9070254d809af129ca8abb0b0ea74ce8819d1
leonardo <pachecodossantos17@gmail.com>
2024-03-01 16:59:05 -0300

    projetoAgenda

---
+++ node/projetoAgenda/server.js
@@ -0,0 +1,61 @@
+require("dotenv").config();
+
+const express = require("express");
+const routes = require("./routes");
+const app = express();
+const mongoose = require("mongoose");
+mongoose
+  .connect(process.env.CONNECTIONSTRING)
+  .then(() => {
+    console.log("Conectei ao banco de dados");
+    app.emit("pronto");
+  })
+  .catch((e) => console.log(e));
+
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+const flash = require("connect-flash");
+const path = require("path");
+const helmet = require("helmet");
+const csrf = require("csurf");
+const {
+  middlewareGlobal,
+  checkCsrfError,
+  csrfMiddleware,
+} = require("./src/middleware/middleware");
+
+app.use(helmet());
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.resolve(__dirname, "public")));
+
+const sessionOptions = session({
+  secret: "umSegredoAqui",
+  store: MongoStore.create({ mongoUrl: process.env.CONNECTIONSTRING }),
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+    httpOnly: true,
+  },
+});
+app.use(sessionOptions);
+app.use(flash());
+
+app.set("views", path.resolve(__dirname, "src", "views"));
+app.set("view engine", "ejs");
+
+app.use(csrf());
+
+app.use(middlewareGlobal);
+app.use(checkCsrfError);
+app.use(csrfMiddleware);
+app.use(routes);
+
+app.on("pronto", () => {
+  app.listen(3000, () => {
+    console.log("O servido esta em http://localhost:3000/");
+    console.log("Esta rodando na porta 3000");
+  });
+});
---
+++ node/projetoAgenda/src/controllers/contatoController.js
@@ -0,0 +1,25 @@
+const Contato = require("../models/contatoModel");
+
+exports.index = (req, res) => {
+  res.render("contato");
+};
+
+exports.register = async (req, res) => {
+  try {
+    const contato = new Contato(req.body);
+    await contato.register();
+
+    if (contato.errors.length > 0) {
+      req.flash("errors", contato.errors);
+      req.session.save(() => res.redirect("back"));
+      return;
+    }
+
+    req.flash("success", "Contato registrado com sucesso");
+    req.session.save(() => res.redirect("back"));
+    return;
+  } catch (e) {
+    console.log(e);
+    res.render("404");
+  }
+};
