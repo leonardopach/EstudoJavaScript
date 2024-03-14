3fbc67774f38fd878480e543d3340817119427a5
leonardo <pachecodossantos17@gmail.com>
2024-03-14 18:01:51 -0300

    react

---
+++ react/listatarefas/.eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: 'airbnb',
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parser: 'babel-eslint',
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'react/jsx-filename-extension': 'off',
+    'react/no-unused-state': 'off',
+    'react/state-in-constructor': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'jsx-a11y/control-has-associated-label': 'off',
+  },
+};
