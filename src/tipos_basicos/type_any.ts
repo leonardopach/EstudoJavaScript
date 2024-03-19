0294468f753b4763e42383e8cc1bfbcaa71814ac
leonardo <pachecodossantos17@gmail.com>
2024-03-19 09:56:50 -0300

    arquivos limpos

--- src/tipos_basicos/type_any.ts
+++
@@ -1,8 +0,0 @@
-// utilizer any em apenas em ultimo caso
-function showMessage(msg: any) {
-  return msg;
-}
-
-console.log(showMessage([1, 2, 3, 5]));
-console.log(showMessage("ola"));
-console.log(showMessage(12));
--- src/tipos_basicos/type_unknown.ts
+++
@@ -1,8 +0,0 @@
-let x: unknown;
-x = 100;
-x = "Leonardo";
-x = 900;
-
-const y = 800;
-
-if (typeof x === "number") console.log(x + y);
